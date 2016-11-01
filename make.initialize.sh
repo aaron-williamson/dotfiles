87264b17eae9370a8f39536f13d1917bf7366198
Aaron Williamson <aarowill@cisco.com>
2016-11-01 15:21:19 -0600

    Update initialize script

--- make.initialize.sh
+++ make.initialize.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+function link_dotfile {
+  dest="${HOME}/.$(basename $1)"
+  if [[ -e $dest ]]; then
+    echo File $dest exists, did not create link
+  else
+    echo Creating link from $1 to $dest
+    ln -s $1 $dest
+  fi
+}
+
 current_dir=$( cd "$( dirname ${BASH_SOURCE[0]} )" && pwd )
 
 # Directory of config files
@@ -9,16 +19,6 @@ config_dir=$current_dir/configs
 
 # Ensure we're in the dotfiles directory
 cd $current_dir
-
-function link_dotfile {
-  dest="${HOME}/.$(basename $1)"
-  if [ -e $dest ]; then
-    echo File $dest exists, did not create link
-  else
-    echo Creating link from $1 to $dest
-    ln -s $1 $dest
-  fi
-}
 
 # Link the dotfiles
 echo Linking dotfiles to home directory
@@ -40,6 +40,30 @@ for file in $prezto_files; do
 done
 
 echo
+
+# Check for neovim
+echo Checking for neovim...
+if (hash nvim 2>/dev/null); then
+  has_neovim=true
+
+  # If we have neovim, link the vim directory to the right place
+  echo Neovim detected
+  if [[ -n $XDG_CONFIG_HOME ]]; then
+    neovim_conf_dir="$XDG_CONFIG_HOME"
+  else
+    neovim_conf_dir="$HOME/.config"
+  fi
+
+  if [[ -e "${neovim_conf_dir}/nvim" ]]; then
+    echo Neovim directory found, skipping linking
+  else
+    echo Linking neovim configuration directory
+    mkdir -p $neovim_conf_dir
+    ln -s $current_dir $neovim_conf_dir/nvim
+  fi
+else
+  echo No neovim detected
+fi
 
 # Set up vim
 echo Running vim setup
