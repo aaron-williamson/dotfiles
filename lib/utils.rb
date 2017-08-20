819015da5a718be997e339019d24b094255460d8
Aaron Williamson <guitarfanman@gmail.com>
2017-08-20 13:38:04 -0600

    Don't use non portable which flag

--- lib/utils.rb
+++ lib/utils.rb
@@ -3,7 +3,7 @@ require 'open3'
 module DotfilesCli
   module Utils
     def executable_in_path?(name)
-      _, status = Open3.capture2('which', '-s', name)
+      _, status = Open3.capture2('which', name)
       status.exitstatus.zero?
     end
   end
