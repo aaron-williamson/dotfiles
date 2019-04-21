5db730fb562dfaca30bb7b4420b9e989310f3c01
Aaron Williamson <guitarfanman@gmail.com>
2019-04-21 16:05:01 -0600

    Updates to git config
    Add vscode diff and merge tool
    Add option for configurable diff/merge tool to git config (defaults to
    what is already in gitconfig and then vimdiff)
    Fix typo in git help docs

--- lib/dotfile_commands/git.rb
+++ lib/dotfile_commands/git.rb
@@ -7,7 +7,9 @@ module DotfilesCli
       desc '[Options]', 'generate gitconfig from template'
       class_option 'git-user', desc: 'The user name to put in the gitconfig'
       class_option 'git-email', desc: 'The user email to put in the gitconfig'
-      class_option 'git-push', desc: 'What value to use for push.default inthe gitconfig'
+      class_option 'git-push', desc: 'What value to use for push.default in the gitconfig'
+      class_option 'git-mergetool', desc: 'What value to use for merge.tool in the gitconfig'
+      class_option 'git-difftool', desc: 'What value to use for diff.tool in the gitconfig'
 
       def setup(*_args)
         return unless executable_in_path?('git')
@@ -22,6 +24,7 @@ module DotfilesCli
         dest_config = File.join(options[:destination], '.gitconfig')
         fancy_diff  = executable_in_path?('diff-so-fancy')
         (user, email) = user_and_email
+        (mergetool, difftool) = tools
 
         # Determine git version
         git_version = Gem::Version.new(Open3.capture2('git --version').first.split("\s").last)
@@ -53,7 +56,9 @@ module DotfilesCli
                  user: user,
                  email: email,
                  fancy_diff: fancy_diff,
-                 decorate_color: decorate_color)
+                 decorate_color: decorate_color,
+                 mergetool: mergetool,
+                 difftool: difftool)
       end
 
       def user_and_email
@@ -62,10 +67,8 @@ module DotfilesCli
         email = options['git-email'] || ''
 
         # Attempt to determine user and/or email from existing config
-        if user.empty? || email.empty?
-          user  = ::Git.global_config('user.name') if user.empty?
-          email = ::Git.global_config('user.email') if email.empty?
-        end
+        user  = ::Git.global_config('user.name') if user.empty?
+        email = ::Git.global_config('user.email') if email.empty?
 
         # Don't create a config without user and email
         if user.empty? || email.empty?
@@ -74,6 +77,22 @@ module DotfilesCli
         end
 
         [user, email]
+      end
+
+      def tools
+        # Set these to the empty string if they're not defined so that we can always use .empty?
+        mergetool = options['git-mergetool'] || ''
+        difftool = options['git-difftool'] || ''
+
+        # Attempt to determine merge/diff tools from existing config
+        mergetool = ::Git.global_config('merge.tool') if mergetool.empty?
+        difftool = ::Git.global_config('diff.tool') if difftool.empty?
+
+        # Default to vimdiff for both merge and diff tools
+        mergetool = 'vimdiff' if mergetool.empty?
+        difftool = 'vimdiff' if difftool.empty?
+
+        [mergetool, difftool]
       end
     end
   end
