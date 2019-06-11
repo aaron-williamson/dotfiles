c35504b4e4a92fa946075edb915c9c86a5e0f157
Aaron Williamson <aaron.williamson@arcurve.com>
2019-06-11 15:59:47 -0600

    Remove gemrc configuration

--- lib/dotfile_commands/ruby.rb
+++ lib/dotfile_commands/ruby.rb
@@ -7,7 +7,6 @@ module DotfilesCli
 
       def setup(*_args)
         create_link File.join(options[:destination], '.irbrc'), File.join(options[:configs], 'irbrc')
-        create_link File.join(options[:destination], '.gemrc'), File.join(options[:configs], 'gemrc')
       end
     end
   end
